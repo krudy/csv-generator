c48195f91633a3e95589f2ac538d23104ea6ab2a
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-10-20 11:07:20 +0200

    homepage view has been added

--- app/app.js
+++ app/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
-
-const port = 9999;
+const { read } = require('fs');
+const path = require('path');
 const app = express();
+const port = 9999;
+const data = require('./data.json');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/../views'));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.post('/', (req, res) => {
+
+});
+
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
